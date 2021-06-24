429f628a94fa66aea4a4735f7842e2dd33589c8a
Melvin Cayas <cayasmj@gmail.com>
2021-06-24 19:11:35 -0400

    Broke out ShowLists into categories

--- client/src/components/ClientView/ClientView.js
+++ client/src/components/ClientView/ClientView.js
@@ -6,9 +6,15 @@ import ListDetail from "./ListDetail/ListDetail";
 
 const UserInterface = () => {
 	const [isMakingList, setIsMakingList] = useState(false);
-
 	const allLists = useSelector(state => state.lists.lists);
 	const selectedListId = useSelector(state => state.lists.selectedListToView);
+
+	const categories = allLists.map(list => list.category);
+	const uniqueCategories = categories.filter((category, index, arr) => {
+		return arr.indexOf(category) === index;
+	});
+	console.log(uniqueCategories);
+
 	const selectedList = allLists.find(list => list._id === selectedListId);
 
 	const newListHandler = event => {
@@ -19,7 +25,9 @@ const UserInterface = () => {
 	return (
 		<Fragment>
 			{isMakingList && <NewList listToggler={newListHandler} />}
-			<ShowLists newListHandler={newListHandler} />
+			{uniqueCategories.map(category => (
+				<ShowLists category={category} newListHandler={newListHandler} />
+			))}
 			{selectedList && <ListDetail selected={selectedList} />}
 		</Fragment>
 	);
--- client/src/components/ClientView/ShowLists/ShowLists.js
+++ client/src/components/ClientView/ShowLists/ShowLists.js
@@ -4,11 +4,14 @@ import Button from "../../UI/Button";
 import ListItem from "./ListItem";
 
 const ShowLists = props => {
-	const lists = useSelector(state => state.lists.lists);
+	const allLists = useSelector(state => state.lists.lists);
+	const groupedList = allLists.filter(list => list.category === props.category);
+	console.log(groupedList);
+
 	let result = <p>Create a list to get started!</p>;
 
-	if (lists.length > 0) {
-		result = lists.map(list => (
+	if (groupedList.length > 0) {
+		result = groupedList.map(list => (
 			<ListItem id={list._id} key={list._id} title={list.title} />
 		));
 	} else {
@@ -16,7 +19,7 @@ const ShowLists = props => {
 	}
 
 	return (
-		<Card header="Lists">
+		<Card header={props.category}>
 			<Button clickHandler={props.newListHandler}>Create List</Button>
 			<section>{result}</section>
 		</Card>
