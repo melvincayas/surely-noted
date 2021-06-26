22bd1b275af5e84a87e6733e76ed773d5b8b4e90
Melvin Cayas <cayasmj@gmail.com>
2021-06-26 19:13:20 -0400

    Added active classes for Sidebar categories

--- client/src/components/ClientView/Sidebar/Category.js
+++ client/src/components/ClientView/Sidebar/Category.js
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import classes from "../ListSelection.module.css";
 
 const Category = props => {
+	const isQueried = props.queryText === props.category ? classes.active : "";
+
 	return (
 		<div className="mb-2">
 			<Link
 				to={`/home?filter=${props.category}`}
-				className={`is-size-5 mb-2 pl-5 ml-5 ${classes.link}`}
+				className={`is-size-5 mb-2 pl-5 ml-5 ${classes.link} ${isQueried}`}
 			>
 				{props.category}
 			</Link>
--- client/src/components/ClientView/Sidebar/Sidebar.js
+++ client/src/components/ClientView/Sidebar/Sidebar.js
@@ -1,18 +1,23 @@
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Category from "./Category";
 import classes from "../ListSelection.module.css";
 
 const Sidebar = ({ categories }) => {
+	const location = useLocation();
+	const query = new URLSearchParams(location.search);
+	const queryText = query.get("filter");
+	const isAll = queryText === null ? classes.active : "";
+
 	return (
 		<Fragment>
 			<div className="mb-2 pl-5 ml-5">
-				<Link to="/home" className={`is-size-5 ${classes.link}`}>
+				<Link to="/home" className={`is-size-5 ${classes.link} ${isAll}`}>
 					All
 				</Link>
 			</div>
 			{categories.map((category, index) => (
-				<Category key={index} category={category} />
+				<Category key={index} category={category} queryText={queryText} />
 			))}
 		</Fragment>
 	);
