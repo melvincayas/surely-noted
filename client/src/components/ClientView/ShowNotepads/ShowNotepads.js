2e2b1726075b6b46c70972ecccdd2204279f568f
Melvin Cayas <cayasmj@gmail.com>
2021-06-26 20:41:08 -0400

    Added onClick handler for ShowNotepads

--- client/src/components/ClientView/ShowNotepads/ShowNotepads.js
+++ client/src/components/ClientView/ShowNotepads/ShowNotepads.js
@@ -1,11 +1,20 @@
+import { useHistory } from "react-router-dom";
 import ShowTitle from "./ShowTitle";
 import ShowContent from "./ShowContent";
 import classes from "./styles/ShowNotepads.module.css";
 
 const ShowNotepads = ({ list }) => {
-	console.log(list);
+	const history = useHistory();
+
+	const onClickHandler = () => {
+		history.push(`/list/${list._id}`);
+	};
+
 	return (
-		<div className={`columns ${classes["notepad-container"]}`}>
+		<div
+			className={`columns ${classes["notepad-container"]}`}
+			onClick={onClickHandler}
+		>
 			<div className="column is-half">
 				<ShowTitle title={list.title} category={list.category} />
 			</div>
