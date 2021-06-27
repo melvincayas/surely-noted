c42389d5622f060c30bb05711b3c59ece603327e
Melvin Cayas <cayasmj@gmail.com>
2021-06-27 18:07:39 -0400

    Added add btn to Notepad

--- client/src/components/ClientView/ListDetail/NotepadLayout.js
+++ client/src/components/ClientView/ListDetail/NotepadLayout.js
@@ -1,11 +1,20 @@
 import classes from "./styles/NotepadLayout.module.css";
 
 const NotepadLayout = props => {
+	const addTaskHandler = () => {
+		alert("clicked");
+	};
+
 	return (
 		<div className={`${classes.notepad} ${props.className}`}>
 			<div className={classes.header}>
-				<h1 className={classes.title}>{props.title}</h1>
-				<h3 className={classes.subtitle}>{props.category}</h3>
+				<div>
+					<h1 className={classes.title}>{props.title}</h1>
+					<h3 className={classes.subtitle}>{props.category}</h3>
+				</div>
+				<div className={classes["notepad-action-btns"]}>
+					<i className="fas fa-plus-square"></i>
+				</div>
 			</div>
 			<section>{props.children}</section>
 		</div>
