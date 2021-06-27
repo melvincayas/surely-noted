54907cba83162f02975f6e0de5d751815527d7a7
Melvin Cayas <cayasmj@gmail.com>
2021-06-26 23:24:42 -0400

    Changed length of Notepad item

--- client/src/components/ClientView/ShowNotepads/ShowStatus.js
+++ client/src/components/ClientView/ShowNotepads/ShowStatus.js
@@ -1,7 +1,7 @@
 const ShowStatus = ({ items }) => {
 	return (
 		<div className="column is-one-quarter">
-			<p className="is-size-5">{items.length} Notes</p>
+			<p className="is-size-5">{items.length} Tasks</p>
 		</div>
 	);
 };
