eea6528295dfd6a95c7cf6de983fedd1fcdde50e
Melvin Cayas <cayasmj@gmail.com>
2021-06-29 18:03:06 -0400

    Fixed /home typo in NotFound

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
 		<div className="has-text-centered pt-5">
 			<i className="fas fa-unlink is-size-1"></i>
 			<p className="is-size-4 my-5">We can't find what you're looking for!</p>
-			<Link className="is-size-4" to="home">
+			<Link className="is-size-4" to="/home">
 				Go Home
 			</Link>
 		</div>
