0eb9c306f94e0e8c9b4394bb725c91a8186e0a22
Melvin Cayas <cayasmj@gmail.com>
2021-07-07 21:16:10 -0400

    Added conditional for props.className in Button

--- client/src/components/UI/Button.js
+++ client/src/components/UI/Button.js
@@ -4,7 +4,9 @@ import classes from "../../styles/UI/Button.module.css";
 const Button = props => {
 	return (
 		<button
-			className={`${classes["base-btn"]} ${props.className}`}
+			className={`${classes["base-btn"]} ${
+				props.className ? props.className : ""
+			}`}
 			disabled={props.isValid}
 			onClick={props.clickHandler}
 		>
