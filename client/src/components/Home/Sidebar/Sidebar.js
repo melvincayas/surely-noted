e5407ad9cec4d4772b08d0fd350a1f32e6a95819
Melvin Cayas <cayasmj@gmail.com>
2021-07-09 18:45:38 -0400

    Styled categories filter box

--- client/src/components/Home/Sidebar/Sidebar.js
+++ client/src/components/Home/Sidebar/Sidebar.js
@@ -8,25 +8,26 @@ const Sidebar = ({ categories }) => {
 	const queryText = query.get("filter");
 	const isAll = queryText === null ? classes.active : "";
 
-	console.log(categories);
-
 	return (
-		<nav className={`column is-3 ${classes.border}`}>
-			<p class="title is-4 pl-5 ml-5">Filter by:</p>
-
-			<ul>
-				<li className="mb-2">
-					<Link
-						to="/home"
-						className={`is-size-5 pl-5 ml-5 ${isAll} ${classes["sidebar-links"]}`}
-					>
-						All
-					</Link>
-				</li>
-				{categories.map((category, index) => (
-					<Category key={index} category={category} queryText={queryText} />
-				))}
-			</ul>
+		<nav className="column is-3">
+			<div className={classes["filter-box"]}>
+				<p className={classes.header}>Categories</p>
+				<ul>
+					{queryText && (
+						<li className="mb-2">
+							<Link
+								to="/home"
+								className={`${isAll} ${classes["sidebar-links"]}`}
+							>
+								Show All
+							</Link>
+						</li>
+					)}
+					{categories.map((category, index) => (
+						<Category key={index} category={category} queryText={queryText} />
+					))}
+				</ul>
+			</div>
 		</nav>
 	);
 };
