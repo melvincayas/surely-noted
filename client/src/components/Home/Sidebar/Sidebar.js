5ca2e57d50982bff076eca92ebeecb8ab1215ee5
Melvin Cayas <cayasmj@gmail.com>
2021-07-09 17:18:12 -0400

    Moved nav into Sidebar

--- client/src/components/Home/Sidebar/Sidebar.js
+++ client/src/components/Home/Sidebar/Sidebar.js
@@ -9,19 +9,22 @@ const Sidebar = ({ categories }) => {
 	const isAll = queryText === null ? classes.active : "";
 
 	return (
-		<ul>
-			<li className="mb-2">
-				<Link
-					to="/home"
-					className={`is-size-5 pl-5 ml-5 ${isAll} ${classes["sidebar-links"]}`}
-				>
-					All
-				</Link>
-			</li>
-			{categories.map((category, index) => (
-				<Category key={index} category={category} queryText={queryText} />
-			))}
-		</ul>
+		<nav className={`column is-3 ${classes.border}`}>
+			<p class="title is-4 pl-5 ml-5">Filter by:</p>
+			<ul>
+				<li className="mb-2">
+					<Link
+						to="/home"
+						className={`is-size-5 pl-5 ml-5 ${isAll} ${classes["sidebar-links"]}`}
+					>
+						All
+					</Link>
+				</li>
+				{categories.map((category, index) => (
+					<Category key={index} category={category} queryText={queryText} />
+				))}
+			</ul>
+		</nav>
 	);
 };
 
