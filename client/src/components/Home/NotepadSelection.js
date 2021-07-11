f791704dd2825311822bccc42b3eb176d682afb3
Melvin Cayas <cayasmj@gmail.com>
2021-07-11 10:39:15 -0400

    Added recently updated sort logic

--- client/src/components/Home/NotepadSelection.js
+++ client/src/components/Home/NotepadSelection.js
@@ -53,6 +53,11 @@ const NotepadSelection = () => {
 			});
 			break;
 		default:
+			sortedNotepadsBeingViewed.sort((a, b) => {
+				if (a.modified > b.modified) return -1;
+				if (a.modified < b.modified) return 1;
+				return 0;
+			});
 			break;
 	}
 
