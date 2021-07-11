ca8666989a16b5e18692913f6c8d05da0fc38239
Melvin Cayas <cayasmj@gmail.com>
2021-07-11 10:40:27 -0400

    Moved new sort up 2 levels in dropdown

--- client/src/components/Home/ShowNotepads/SortDropdown.js
+++ client/src/components/Home/ShowNotepads/SortDropdown.js
@@ -7,9 +7,9 @@ const SortDropdown = ({ sortValue, onChangeHandler }) => {
 			className="mb-5 ml-2"
 		>
 			<option value="recent">Recently Updated</option>
+			<option value="newest">Newest</option>
 			<option value="ascending">Name: A-Z</option>
 			<option value="descending">Name: Z-A</option>
-			<option value="newest">Newest</option>
 		</select>
 	);
 };
