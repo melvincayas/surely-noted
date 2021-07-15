3d2bc53e6ddf171475cf5bbd4cbff807f9777427
Melvin Cayas <cayasmj@gmail.com>
2021-07-15 16:52:01 -0400

    Added number of completed tasks and made font bigger

--- client/src/components/Home/ShowNotepads/ShowStatus.js
+++ client/src/components/Home/ShowNotepads/ShowStatus.js
@@ -1,9 +1,12 @@
 const ShowStatus = ({ items }) => {
+	const completedTasks = items.filter(task => task.complete);
+
 	return (
 		<div className="column is-one-quarter">
-			<p className="is-size-5">
+			<p className="title is-4 mb-0">
 				{items.length} {items.length === 1 ? "Task" : "Tasks"}
 			</p>
+			<p className="is-size-5">{completedTasks.length} Done</p>
 		</div>
 	);
 };
