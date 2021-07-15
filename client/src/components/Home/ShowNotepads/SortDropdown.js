16cd29a9c8e4417cf0e91f093f0e71b33bbc4226
Melvin Cayas <cayasmj@gmail.com>
2021-07-15 17:17:25 -0400

    Increase font-size of sort dropdown and removed outline

--- client/src/components/Home/ShowNotepads/SortDropdown.js
+++ client/src/components/Home/ShowNotepads/SortDropdown.js
@@ -1,10 +1,12 @@
+import classes from "../../../styles/Home/ShowNotepads.module.css";
+
 const SortDropdown = ({ sortValue, onChangeHandler }) => {
 	return (
 		<select
 			value={sortValue}
 			onChange={onChangeHandler}
 			name="sort"
-			className="mb-5 ml-2"
+			className={`mb-5 ml-2 ${classes["sort-dropdown"]}`}
 		>
 			<option value="recent">Recently Updated</option>
 			<option value="newest">Newest</option>
