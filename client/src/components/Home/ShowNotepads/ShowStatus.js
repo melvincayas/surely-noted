7b8f5a467e48c55467f637d4676c71fc775dc8af
Melvin Cayas <cayasmj@gmail.com>
2021-07-15 16:38:07 -0400

    Added conditional for Tasks in ShowStatus

--- client/src/components/Home/ShowNotepads/ShowStatus.js
+++ client/src/components/Home/ShowNotepads/ShowStatus.js
@@ -1,7 +1,9 @@
 const ShowStatus = ({ items }) => {
 	return (
 		<div className="column is-one-quarter">
-			<p className="is-size-5">{items.length} Tasks</p>
+			<p className="is-size-5">
+				{items.length} {items.length === 1 ? "Task" : "Tasks"}
+			</p>
 		</div>
 	);
 };
