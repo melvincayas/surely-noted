f2fbc6cb584c88ae297c97c48b28cbb97de3f4c8
Melvin Cayas <cayasmj@gmail.com>
2021-07-16 12:25:30 -0400

    Added email input into share modal

--- client/src/hooks/useShare.js
+++ client/src/hooks/useShare.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 import { useDispatch } from "react-redux";
 import Modal from "../components/UI/Modal";
 import Button from "../components/UI/Button";
+import Input from "../components/UI/Input";
 import modalClasses from "../styles/UI/Modal.module.css";
 import btnClasses from "../styles/UI/Button.module.css";
+import useInputValidation from "./useInputValidation";
+import inputStyles from "../styles/UI/Input.module.css";
 
 // useState for opening share modal onClick of Share in dropdown
 // make modal where user will enter e-mail to find
@@ -13,6 +16,13 @@ import btnClasses from "../styles/UI/Button.module.css";
 
 const useShare = notepadId => {
 	const [isShowingShareModal, setIsShowingShareModal] = useState(false);
+	const {
+		inputChangeHandler: emailChangeHandler,
+		inputBlurHandler: emailBlurHandler,
+		isInputValid: emailIsValid,
+		inputHasError: emailError,
+		value: enteredEmail,
+	} = useInputValidation(input => input.includes("@") && input.includes("."));
 	const dispatch = useDispatch();
 
 	const shareClickHandler = event => {
@@ -25,6 +35,15 @@ const useShare = notepadId => {
 			<p className={modalClasses.message}>
 				Enter e-mail(s) of people to share your notepad with!
 			</p>
+			<Input
+				onChangeHandler={emailChangeHandler}
+				isInputValid={emailIsValid}
+				onBlurHandler={emailBlurHandler}
+				className={emailError ? inputStyles["input-invalid"] : ""}
+				id="email"
+				name="email"
+				type="email"
+			/>
 			<div className={modalClasses["btn-container"]}>
 				<Button clickHandler={shareClickHandler}>Share</Button>
 				<Button className={btnClasses.cancel} clickHandler={shareClickHandler}>
