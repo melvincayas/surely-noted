ebc85287985e7503aa8a7fced21c907ca87abb23
Melvin Cayas <cayasmj@gmail.com>
2021-07-17 15:32:26 -0400

    Added logic for shared notepads in middleware validation

--- server/public/utilities/middleware.js
+++ server/public/utilities/middleware.js
@@ -3,14 +3,16 @@ const User = require("../../models/User");
 const catchAsync = require("./catchAsync");
 const ErrorHandler = require("./ErrorHandler");
 
-// this might not be necessary
-// NotepadDetail only gets notepads in user's collection
 module.exports.validateNotepadUser = catchAsync(async (req, res, next) => {
 	const { user_id } = req.session;
 	const { notepadId } = req.params;
-	const { creator } = await Notepad.findOne({ _id: notepadId });
+	const { creator, shared } = await Notepad.findOne({ _id: notepadId });
 
-	if (user_id !== creator.toString()) {
+	const userIsBeingSharedWith = shared.find(
+		user => user.toString() === user_id.toString()
+	);
+
+	if (user_id !== creator.toString() && !userIsBeingSharedWith) {
 		return next(new ErrorHandler(403, "This isn't yours!", "authorization"));
 	}
 
