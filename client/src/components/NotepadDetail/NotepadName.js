006e4059100b11d78a0ddc7cea3d324c71d42742
Melvin Cayas <cayasmj@gmail.com>
2021-07-19 12:57:02 -0400

    Added styling for shared badge

--- client/src/components/NotepadDetail/NotepadName.js
+++ client/src/components/NotepadDetail/NotepadName.js
@@ -3,13 +3,16 @@ import classes from "../../styles/NotepadDetail/NotepadLayout.module.css";
 const NotepadName = ({ title, category, shared }) => {
 	const isSharedNotepad = shared.length > 0;
 
-	const sharedNotepadButton = <button>shared</button>;
+	const sharedNotepadButton = (
+		<button className={classes["shared-notepad-badge"]}>shared</button>
+	);
 
 	return (
 		<div>
-			<h1 className={classes.title}>
-				{title} {isSharedNotepad && sharedNotepadButton}
-			</h1>
+			<div className={classes["title-container"]}>
+				<h1 className={classes.title}>{title}</h1>
+				{isSharedNotepad && sharedNotepadButton}
+			</div>
 			<h3 className={classes.subtitle}>{category}</h3>
 		</div>
 	);
