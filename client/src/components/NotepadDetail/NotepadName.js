bbe92b70d5eb8e06851da2c7a7e197d5b0a2474c
Melvin Cayas <cayasmj@gmail.com>
2021-07-19 17:03:26 -0400

    Added Modal to NotepadName for shared

--- client/src/components/NotepadDetail/NotepadName.js
+++ client/src/components/NotepadDetail/NotepadName.js
@@ -1,20 +1,44 @@
+import { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+import Modal from "../UI/Modal";
+import Button from "../UI/Button";
 import classes from "../../styles/NotepadDetail/NotepadLayout.module.css";
 
 const NotepadName = ({ title, category, shared }) => {
+	const [sharedUsersModalOpen, setSharedUsersModalOpen] = useState(false);
 	const isSharedNotepad = shared.length > 0;
 
+	const toggleSharedUsersModal = () => {
+		setSharedUsersModalOpen(prevState => !prevState);
+	};
+
 	const sharedNotepadButton = (
-		<button className={classes["shared-notepad-badge"]}>shared</button>
+		<button
+			className={classes["shared-notepad-badge"]}
+			onClick={toggleSharedUsersModal}
+		>
+			shared
+		</button>
+	);
+
+	const sharedUsersModal = ReactDOM.createPortal(
+		<Modal clickHandler={toggleSharedUsersModal} header="Shared Users">
+			Test
+		</Modal>,
+		document.getElementById("root")
 	);
 
 	return (
-		<div>
-			<div className={classes["title-container"]}>
-				<h1 className={classes.title}>{title}</h1>
-				{isSharedNotepad && sharedNotepadButton}
+		<Fragment>
+			{sharedUsersModalOpen && sharedUsersModal}
+			<div>
+				<div className={classes["title-container"]}>
+					<h1 className={classes.title}>{title}</h1>
+					{isSharedNotepad && sharedNotepadButton}
+				</div>
+				<h3 className={classes.subtitle}>{category}</h3>
 			</div>
-			<h3 className={classes.subtitle}>{category}</h3>
-		</div>
+		</Fragment>
 	);
 };
 
